d91f0ded772898b23ec04de73d174019af5c2903
Sankalp Rai <sankalp137rai@gmail.com>
2024-03-21 10:36:02 +0530

    added doctor precription component in UI

--- src/DoctorComponents/Prescription.js
+++ src/DoctorComponents/Prescription.js
@@ -266,25 +266,6 @@ const Prescription = () => {
                 )}
             </div>
             {/* show patient history */}
-            <div className='relative z-0 w-full mb-6 group'>
-                <div>
-                    <label className="relative inline-flex items-center cursor-pointer">
-                        <input
-                            type="checkbox"
-                            value=""
-                            className="sr-only peer"
-                            checked={showHistory}
-                            onChange={handleHistory}
-                            placeholder='Show Patient Medical History'
-                        />
-                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600"></div>
-                        <span className="ml-3 text-sm font-normal text-gray-900 dark:text-gray-800">
-                            Show Patient E-Aarogya Medical History
-                        </span>
-                    </label>
-                </div>
-                {showHistory && (<PatientMedicalHistory patientDetail={patientDetail} />)}
-            </div>
             <div className=" flex flex-col border-2 rounded-lg border-orange-600 items-center justify-center p-4">
                 <div className='flex flex-row items-center space-x-2 px-8'>
                     <p className='py-2 text-sm text-gray-900 font-normal'>Patient's Uploaded Health Records</p>
